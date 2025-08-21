494f933f84df2df6e41fade418edafaf0fd2430a
Gabi Gio <gabrielagiovanna3007@gmail.com>
2025-08-21 17:56:31 -0300

    🧪 test(cypress): move visit() p/ beforeEach e testa envio do formulário

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,6 +1,18 @@
 describe("Central de Atendimento ao Cliente TAT", () => {
-  it("verifica o título da aplicação", () => {
+  beforeEach(() => {
     cy.visit("./src/index.html");
+  });
+
+  it("verifica o título da aplicação", () => {
     cy.title().should("be.equal", "Central de Atendimento ao Cliente TAT");
+  });
+
+  it("preenche os campos obrigatórios e envia o formulário", () => {
+    cy.get("#firstName").type("Gabriela");
+    cy.get("#lastName").type("Giovanna Silva");
+    cy.get("#email").type("gabi@gmail.com");
+    cy.get("#open-text-area").type("Duvida sobre o tema de um curso");
+    cy.get(".button").click();
+    cy.get(".success").should("be.visible");
   });
 });
