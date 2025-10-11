742e5470e73495630bc83f8db27c65e9909e7268
Gabi Gio <gabrielagiovanna3007@gmail.com>
2025-10-11 12:32:37 -0300

    🧪 test: adiciona validação para impedir valor não numérico no campo de telefone (exercício extra 3)

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -22,12 +22,16 @@ describe("Central de Atendimento ao Cliente TAT", () => {
     cy.get(".success").should("be.visible");
   });
 
-  it.only("exibe mensagem de erro ao submeter o formulário com um email com formatação inválida", () => {
+  it("exibe mensagem de erro ao submeter o formulário com um email com formatação inválida", () => {
     cy.get("#firstName").type("Gabriela");
     cy.get("#lastName").type("Giovanna Silva");
     cy.get("#email").type("gabicom");
     cy.get("#open-text-area").type("Teste");
     cy.get('button[type="submit"]').click();
     cy.get(".error").should("be.visible");
+  });
+
+  it.only("campo telefone continua vazio quando preenchido com valor não numérico", () => {
+    cy.get("#phone").type("abcdefghij").should("have.value", "");
   });
 });
