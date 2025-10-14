ace03f8f39e2231fa4e6399df9ae668d2648a3a4
Gabi Gio <gabrielagiovanna3007@gmail.com>
2025-10-14 16:46:17 -0300

    🧪 test(cypress): adiciona teste que marca o tipo de atendimento "Feedback" e valida seleção com .check()

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -94,17 +94,17 @@ describe("Central de Atendimento ao Cliente TAT", () => {
     cy.get("#product").select("YouTube").should("have.value", "youtube");
   });
 
-  it("seleciona um produto (Mentoria) por seu valor (value)",() => {
-
-
-cy.get("#product").select("mentoria").should("have.value","mentoria")
-
-  })
-
-  it.only("seleciona um produto (Blog) por seu índice", () =>{
-
-cy.get('#product').select(1).should("have.value","blog")
-
+  it("seleciona um produto (Mentoria) por seu valor (value)", () => {
+    cy.get("#product").select("mentoria").should("have.value", "mentoria");
   });
 
+  it("seleciona um produto (Blog) por seu índice", () => {
+    cy.get("#product").select(1).should("have.value", "blog");
+  });
+
+  it.only('marca o tipo de atendimento "Feedback"', () => {
+    cy.get('input[type="radio"][value="feedback"]')
+      .check()
+      .should("be.checked");
+  });
 });
